c7f2b216e74b1cb784f3b84466ee60f837c28b72
Tae Hui Chang <jasmine878@yahoo.com>
2019-10-24 16:56:04 -0400

    Create a pop method to Single Linked List class

--- Single_Linked_List.js
+++ Single_Linked_List.js
@@ -28,35 +28,79 @@ class SingleLinkedList {
     this.tail = null;
     this.length = 0;
   }
-  addNode(val) {
-    const newNode = new Node(val)
+
+  push(val) {
+    const newNode = new Node(val);
 
     //add 1st Node to Single Linked List
     if (this.head === null) {
       this.head = newNode;
       this.tail = newNode;
-    //add additional nodes to Single Linked List
-    //we set the tail.next to the new node
-    //then update the tail to the new node
-    //the old tail becomes a middle node
+      //add additional nodes to Single Linked List
+      //we set the tail.next to the new node
+      //then update the tail to the new node
+      //the old tail becomes a middle node
     } else {
-      this.tail.next = newNode
-      this.tail = newNode
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
 
-    this.length++
-    return this
+    this.length++;
+    return this;
+  }
+
+  //to delete the last node in a Single Linked List we need to traverse it
+  //we need to start at the beginning of the Single Linked List and move to the last node
+  // traverse() {
+  //   let currentNode = this.head;
+  //   //loop until the next node equals null
+  //   while (currentNode) {
+  //     console.log(currentNode.val)
+  //     currentNode = currentNode.next;
+  //   }
+
+  pop() {
+    //if there are no nodes in the linked list, return undefined
+    if (this.head === null) return undefined
+
+    let currentNode = this.head;
+    let previousNode = null;
+
+    //if there is only 1 node in the linked list, link list should be empty
+    if (currentNode.next === null) {
+      this.head === null
+      this.tail === null
+
+      return currentNode
+    }
+
+    while (currentNode.next) {
+      previousNode = currentNode
+      currentNode = currentNode.next
+    }
+
+    this.tail = previousNode
+    this.tail.next = null
+    this.length--
+
+    //return the removed Node
+    return currentNode
   }
 }
 
-let newList = new SingleLinkedList()
-newList.addNode(1)
-newList.addNode(2)
-newList.addNode(3)
-newList.addNode(4)
-newList.addNode(5)
 
-console.log(newList)
+let newList = new SingleLinkedList()
+newList.push(1)
+newList.push(2)
+newList.push(3)
+newList.push(4)
+newList.push(5)
+// console.log(newList)
+
+newList.pop()
+newList.pop();
+console.log(newList);
+// newList.traverse()
 
 
 
@@ -80,32 +124,60 @@ console.log(newList)
 //     this.tail = null;
 //     this.length = 0;
 //   }
-//   addNode(val) {
-//     const newNode = new Node(val)
+//   push(val) {
+//     const newNode = new Node(val);
 
 //     //add 1st Node to Single Linked List
 //     if (this.head === null) {
 //       this.head = newNode;
-//     //add additional nodes to Single Linked List
-//     //we set the tail.next to the new node
+//       //add additional nodes to Single Linked List
+//       //we set the tail.next to the new node
 //     } else {
-//       this.tail.next = newNode
+//       this.tail.next = newNode;
 //     }
 //     //then update the tail to the new node
 //     //the old tail becomes a middle node
 //     //we always add nodes to the end/ tail of the Single Linked List
-//     this.tail = newNode
-//     this.length++
+//     this.tail = newNode;
+//     this.length++;
 
-//     return this
+//     return this;
+//   }
+
+//   pop() {
+//     //if there are no nodes in the linked list, return undefined
+//     if (this.head === null) return undefined;
+
+//     let currentNode = this.head;
+//     let previousNode = null;
+
+//     //if there is only 1 node in the linked list, link list should be empty
+//     if (currentNode.next === null) {
+//       this.head === null;
+//       this.tail === null;
+//     } else {
+//       while (currentNode.next) {
+//         previousNode = currentNode;
+//         currentNode = currentNode.next;
+//       }
+
+//       this.tail = previousNode;
+//       this.tail.next = null;
+//     }
+//     this.length--;
+//     //return the removed Node
+//     return currentNode;
 //   }
 // }
 
-// let newList = new SingleLinkedList()
-// newList.addNode(1)
-// newList.addNode(2)
-// newList.addNode(3)
-// newList.addNode(4)
-// newList.addNode(5)
+// let newList = new SingleLinkedList();
+// newList.push(1);
+// newList.push(2);
+// newList.push(3);
+// newList.push(4);
+// newList.push(5);
+// // console.log(newList)
 
-// console.log(newList)
+// newList.pop();
+// newList.pop();
+// console.log(newList);
