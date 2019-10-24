76cbd92245fe1a3e1b7c1df6fc4e8cab18a16525
Tae Hui Chang <jasmine878@yahoo.com>
2019-10-24 07:08:33 -0400

    Create a Single Linked List class

--- Single_Linked_List.js
+++ Single_Linked_List.js
@@ -1,4 +1,4 @@
-//1st Node
+//first Node
 class Node {
   constructor(val) {
     this.val = val;
@@ -6,3 +6,26 @@ class Node {
   }
 }
 
+//as we add more Nodes to a Single Linked List,
+//we update the next property of the last Node
+// let firstNode = new Node(1)
+// firstNode.next = new Node(2)
+// firstNode.next.next = new Node(3)
+// firstNode.next.next.next = new Node(4)
+// firstNode.next.next.next.next = new Node(5)
+
+// console.log("1st Node", firstNode);
+// console.log("2nd Node", firstNode.next)
+// console.log("3rd Node", firstNode.next.next)
+// console.log("4th Node", firstNode.next.next.next)
+// console.log("5th Node", firstNode.next.next.next.next)
+
+
+//a single Linked List has a head, tail, and length properties
+class SingleLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
