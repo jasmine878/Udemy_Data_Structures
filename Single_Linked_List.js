b048c11ebb21b132c947c5a17e095b1b782b8e0f
Tae Hui Chang <jasmine878@yahoo.com>
2019-10-24 07:36:38 -0400

    Create Add Node Method to Single Linked List class

--- Single_Linked_List.js
+++ Single_Linked_List.js
@@ -21,11 +21,39 @@ class Node {
 // console.log("5th Node", firstNode.next.next.next.next)
 
 
-//a single Linked List has a head, tail, and length properties
+//a Single Linked List has a head, tail, and length properties
 class SingleLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
+  addNode(val) {
+    const newNode = new Node(val)
+
+    //add 1st Node to Single Linked List
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    //add 2nd Node to Single Linked List
+    } else if (this.head.next === null) {
+      this.head.next = newNode
+      this.tail = newNode
+    //add additional 3rd+ Node to Single Linked List
+    } else {
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+
+    this.length++
+  }
 }
+
+let newList = new SingleLinkedList()
+newList.addNode(1)
+newList.addNode(2)
+newList.addNode(3)
+newList.addNode(4)
+newList.addNode(5)
+
+console.log(newList)
