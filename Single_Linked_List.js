8f915bf97048b7541e55ea49aaf0532017fde407
Tae Hui Chang <jasmine878@yahoo.com>
2019-10-25 10:46:24 -0400

    Create get method to Single Linked List class

--- Single_Linked_List.js
+++ Single_Linked_List.js
@@ -29,7 +29,7 @@ class SingleLinkedList {
     this.length = 0;
   }
 
-  //method that adds a node to the end of a linked list
+  //a method that adds a node to the end of a linked list
   push(val) {
     const newNode = new Node(val);
 
@@ -60,7 +60,7 @@ class SingleLinkedList {
   //     currentNode = currentNode.next;
   //   }
 
-  //method that removes a node from the end of a linked list
+  //a method that removes a node from the end of a linked list
   pop() {
     //if there are no nodes in the linked list, return undefined
     if (this.head === null) return undefined;
@@ -72,7 +72,7 @@ class SingleLinkedList {
     if (currentNode.next === null) {
       this.head = null;
       this.tail = null;
-      this.length--
+      this.length--;
 
       return currentNode;
     }
@@ -90,7 +90,7 @@ class SingleLinkedList {
     return currentNode;
   }
 
-  //method that adds a node to the front of a linked list
+  //a method that adds a node to the front of a linked list
   unshift(val) {
     const newNode = new Node(val);
 
@@ -106,24 +106,44 @@ class SingleLinkedList {
     return this;
   }
 
-  //method that removes a node from the front of a linked list
+  //a method that removes a node from the front of a linked list
   shift() {
     if (this.head === null) return undefined;
 
     const removedNode = this.head;
-    const secondNode = this.head.next
+    const secondNode = this.head.next;
 
     //if there is only 1 node in the linked list, link list should be empty
     if (secondNode === null) {
-      this.head = null
-      this.tail = null
+      this.head = null;
+      this.tail = null;
     } else {
       //reassign the head to the second Node and remove the first node
-      this.head = secondNode
+      this.head = secondNode;
     }
-    this.length--
+    this.length--;
 
-    return removedNode
+    return removedNode;
+  }
+
+  //a method that gets a node at a specific location in a linked list
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return null
+    }
+    let currentNode = this.head
+    // let currentIndex = currentNode.length
+    //cannot use currentNode.length, because the length property measures the length of the linked list, not the currentNode.
+    //The length of a currentNode returns undefined.
+    //Therefore, use a counter instead
+    let counter = 0
+
+    while(counter < index) {
+      currentNode = currentNode.next
+      counter++
+    }
+
+    return currentNode
   }
 }
 
@@ -143,6 +163,7 @@ newList.pop();
 // console.log(newList);    //expect undefined
 
 newList.unshift(4)
+// console.log(newList)        //expect 1 nodes
 newList.unshift(5)
 // console.log(newList)        //expect 2 nodes(5, 4)
 
@@ -152,6 +173,16 @@ newList.shift()
 // console.log(newList);       //expect 0 node
 newList.shift()
 // console.log(newList);       //expect undefined
+
+newList.unshift(1)
+newList.unshift(2)
+newList.unshift(3)
+// console.log(newList)     //expect 3 nodes(3, 2, 1)
+newList.get(0)              //expect 1 node (val 3)
+newList.get(1)              //expect 1 node (val 2)
+newList.get(2)              //expect 1 node (val 1)
+newList.get(3)              //expect null
+
 
 
 // newList.traverse()
@@ -163,75 +194,3 @@ newList.shift()
 
 
 //REFACTORED AND STREAMLINED
-//first Node
-// class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.next = null;
-//   }
-// }
-
-// //a Single Linked List has a head, tail, and length properties
-// class SingleLinkedList {
-//   constructor() {
-//     this.head = null;
-//     this.tail = null;
-//     this.length = 0;
-//   }
-//   push(val) {
-//     const newNode = new Node(val);
-
-//     //add 1st Node to Single Linked List
-//     if (this.head === null) {
-//       this.head = newNode;
-//       //add additional nodes to Single Linked List
-//       //we set the tail.next to the new node
-//     } else {
-//       this.tail.next = newNode;
-//     }
-//     //then update the tail to the new node
-//     //the old tail becomes a middle node
-//     //we always add nodes to the end/ tail of the Single Linked List
-//     this.tail = newNode;
-//     this.length++;
-
-//     return this;
-//   }
-
-//   pop() {
-//     //if there are no nodes in the linked list, return undefined
-//     if (this.head === null) return undefined;
-
-//     let currentNode = this.head;
-//     let previousNode = null;
-
-//     //if there is only 1 node in the linked list, link list should be empty
-//     if (currentNode.next === null) {
-//       this.head === null;
-//       this.tail === null;
-//     } else {
-//       while (currentNode.next) {
-//         previousNode = currentNode;
-//         currentNode = currentNode.next;
-//       }
-
-//       this.tail = previousNode;
-//       this.tail.next = null;
-//     }
-//     this.length--;
-//     //return the removed Node
-//     return currentNode;
-//   }
-// }
-
-// let newList = new SingleLinkedList();
-// newList.push(1);
-// newList.push(2);
-// newList.push(3);
-// newList.push(4);
-// newList.push(5);
-// // console.log(newList)
-
-// newList.pop();
-// newList.pop();
-// console.log(newList);
