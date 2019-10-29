e1f9fcb9b423c1491bd077ed38f56195d03f3554
Tae Hui Chang <jasmine878@yahoo.com>
2019-10-29 12:19:59 -0400

    Create node and double linked list classes

---
+++ Double_Linked_List.js
@@ -0,0 +1,18 @@
+//first node
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+    this.previous = null
+  }
+}
+
+class DoubleLinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+
+}
