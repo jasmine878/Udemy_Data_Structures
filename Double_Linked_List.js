2ed1c762102418ff76ff81bdba2563b218df7948
Tae Hui Chang <jasmine878@yahoo.com>
2019-10-29 21:17:22 -0400

    Create set method to Double Linked List Class

--- Double_Linked_List.js
+++ Double_Linked_List.js
@@ -121,6 +121,18 @@ class DoubleLinkedList {
 
     return currentNode
   }
+
+  set(index, val) {
+    let foundNode = this.get(index)
+
+    if (foundNode) {
+      foundNode.val = val
+      return true
+    }
+
+    return false
+  }
+
 }
 
 let newList = new DoubleLinkedList()
