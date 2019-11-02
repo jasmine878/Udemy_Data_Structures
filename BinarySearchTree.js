884d796fb073466dd20567e82655516b16e228c8
Tae Hui Chang <jasmine878@yahoo.com>
2019-11-02 16:07:23 -0400

    Create Node class and Binary search tree class

---
+++ BinarySearchTree.js
@@ -0,0 +1,16 @@
+//first Node
+class Node {
+  constructor(val) {
+    this.val = val
+    this.left = null
+    this.right = null
+  }
+}
+
+//similar to a Single Linked List
+class BinarySearchTree {
+  constructor() {
+    this.root = null
+    this.size = 0
+  }
+}
