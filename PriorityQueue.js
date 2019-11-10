c01f424b49200d204e32250c5302f14e3a46161c
Tae Hui Chang <jasmine878@yahoo.com>
2019-11-09 21:17:55 -0500

    Create a Priority Queue class

---
+++ PriorityQueue.js
@@ -0,0 +1,14 @@
+//we're only using the priority to compare each node
+class Node {
+  constructor(val, priority) {
+    this.val = val
+    this.priority = priority
+  }
+}
+
+//implement using a Min Binary Heap
+class PriorityQueue {
+  constructor() {
+    this.values = []
+  }
+}
